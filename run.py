f42ec464b035327196f95b8ab7d3f78f11d4a0f5
Ron <ronaldgilesrowe@gmail.com>
2020-03-23 13:04:28 -0500

    Changed file edit method

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import requests
 feedback_list = []
 feedback_form = {'name': " ", 'weight': " ", 'description': " ", 'image_name': " "}
 folder = './supplier-data/descriptions'
-image_folder = './supplier-data/iamges'
+image_folder = './supplier-data/images'
 for file in os.listdir(folder):
   if os.path.isfile(os.path.join(folder, file)):
     with open(folder+'/'+file, 'r') as feedback:
@@ -14,7 +14,7 @@ for file in os.listdir(folder):
      weight = feedback.readline().strip()
      feedback_form['weight'] = int(weight.strip(' lbs'))
      feedback_form['description'] = feedback.readline().strip()
-     feedback_form['image_name'] = image_folder+'/'+file.strip('txt')+'jpeg'
+     feedback_form['image_name'] = file[:4]+'jpeg'
      response = request.post("http://35.223.128.250/fruits", data=feedback_form)
      print(response.status_code)
      print(response.raise_for_status())
